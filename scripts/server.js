50e269147ae15729353680e78a720a36ad0d9ccd
Jonas Rosland <jrosland@vmware.com>
2021-01-23 20:31:02 -0500

    Adding variable ports to server.js

--- scripts/server.js
+++ scripts/server.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use('/', express.static(path.resolve(__dirname, '..')));
 app.use(middleware(compiler, { publicPath: '/dist', writeToDisk: true }));
 
-module.exports = app.listen(3000, () => {
-  console.log('Server is running on the port no. 3000');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+module.exports = app.listen(server_port, server_host, () => {
+  console.log('Listening on port %d', server_port);
 });
